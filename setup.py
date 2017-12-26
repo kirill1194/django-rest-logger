d0ba52166d9e7c4aa55cbfac8de90c07cdbc7291
Кирилл <kirill.altuhov@gmail.com>
2017-12-26 15:52:40 +0500

    fix setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os.path import join, dirname
 
 setup(
     name='djangorestlogger',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.md')).read(),
     install_requires=[
